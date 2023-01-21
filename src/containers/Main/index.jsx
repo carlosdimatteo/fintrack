a82cbf18227b61263d5e8266c2f06a4c61c8a4f9
Carlos Di Matteo <carlosdima97@gmail.com>
2023-01-20 21:39:02 -0500

    added api operation code

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,29 +1,41 @@
-import { useEffect, useState } from "react";
-import { Input } from "../../components/Input";
-import { Title, MainContainer } from "./Main.styles";
-import { Button } from "../../components/Button";
+import { useEffect, useState } from 'react';
+import { Input } from '../../components/Input';
+import { Title, MainContainer } from './Main.styles';
+import { Button } from '../../components/Button';
+import { appendExpenseRowToSheet } from '../../api/google-sheets';
 
 export function Main() {
-  const [username, setUsername] = useState("");
-  useEffect(() => {
-    console.log("loaded");
-  }, []);
+	const [username, setUsername] = useState('');
+	useEffect(() => {
+		console.log('loaded');
+	}, []);
 
-  return (
-    <MainContainer>
-      <Title>{`Track your expense`}</Title>
-      <Input
-        placeholder="Input the value"
-        value={username}
-        onChange={setUsername}
-      />
-      <Button
-        onClick={() => {
-          console.log("clicked");
-        }}
-      >
-        TRACK!!!
-      </Button>
-    </MainContainer>
-  );
+	/**  function handleApiSubmit() {
+		appendExpenseRowToSheet()
+			.then((data) => {
+				succesfully appended mostrar al usuario que esta cuca y reiniciar los inputs 
+			})
+			.catch((error) => {
+				console.log({ error });
+        darle feedback al usuario de que la verga no sirvio y con un mensaje de error pa ver que cono pasa 
+			});
+	} */
+
+	return (
+		<MainContainer>
+			<Title>{`Track your expense`}</Title>
+			<Input
+				placeholder="Input the value"
+				value={username}
+				onChange={setUsername}
+			/>
+			<Button
+				onClick={() => {
+					console.log('clicked');
+				}}
+			>
+				TRACK!!!
+			</Button>
+		</MainContainer>
+	);
 }
