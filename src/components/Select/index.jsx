80bba2638ccfed77b80c1e4a2ca46f39e7bb4eac
Carlos Di Matteo <carlosdima97@gmail.com>
2023-05-01 15:14:15 -0500

    fix categories, added loading, added error handling

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,48 +1,47 @@
-import Select from 'react-select'
-import { StyledDiv } from "./Select.styles";
+import Select from 'react-select';
+import { StyledDiv } from './Select.styles';
 
 const options = [
-  {value: 'alojamiento', label: 'Alojamiento'},
-  {value: 'comida', label: 'Comida'},
-  {value: 'viajes', label: 'Viajes'},
-  {value: 'bienestar', label: 'Bienestar'},
-  {value: 'salud', label: 'Salud'},
-  {value: 'ocio y entretenimiento', label: 'Ocio y Entretenimiento'},
-  {value: 'transporte', label: 'Transporte'},
-  {value: 'bienes materiales', label: 'Bienes Materiales'},
-  {value: 'prestamos e inversiones', label: 'Prestamos e Inversiones'},
-  ] 
-
+	{ value: 'Alojamiento', label: 'Alojamiento' },
+	{ value: 'Comida', label: 'Comida' },
+	{ value: 'Viajes', label: 'Viajes' },
+	{ value: 'Bienestar', label: 'Bienestar' },
+	{ value: 'Salud', label: 'Salud' },
+	{
+		value: 'Ocio y Entretenimiento (salidas y eventos)',
+		label: 'Ocio y Entretenimiento',
+	},
+	{ value: 'Transporte (taxis)', label: 'Transporte' },
+	{ value: 'Bienes materiales (shopping)', label: 'Bienes Materiales' },
+	{ value: 'Prestamos', label: 'Prestamos e Inversiones' },
+];
 
 const colorStyles = {
-  control: (styles) => ({
-    ...styles,
-    height: "45px",
-    width: "calc(400px * .7);",
-    borderRadius: "10px",
-    border: "1px solid transparent",
-    margin: "10px 0 30px 0",
-    fontFamily: "Barlow",
-    
-  }),
-  option: (styles, { data }) => {
-    return { ...styles, color: "#070606", height: "50%", };
-  },
+	control: (styles) => ({
+		...styles,
+		height: '45px',
+		width: 'calc(400px * .7);',
+		borderRadius: '10px',
+		border: '1px solid transparent',
+		margin: '10px 0 30px 0',
+		fontFamily: 'Barlow',
+	}),
+	option: (styles, { data }) => {
+		return { ...styles, color: '#070606', height: '50%' };
+	},
+};
+
+export function SelectComp({ onChange, defaultValue, value }) {
+	return (
+		<StyledDiv>
+			<Select
+				onChange={onChange}
+				defaultValue={defaultValue}
+				value={value}
+				onFocus={(e) => (e.target.readOnly = true)}
+				options={options}
+				styles={colorStyles}
+			/>
+		</StyledDiv>
+	);
 }
-
-export function SelectComp({ onChange, defaultValue, value}) {
-  return (
-    <StyledDiv>
-      <Select
-        onChange={onChange}   
-        defaultValue={defaultValue}
-        value={value}
-        onFocus={(e) => e.target.readOnly = true}
-        options={options}
-        styles={colorStyles}
-      />
-    </StyledDiv>
-
-  );
-}
-
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
-import Axios from "axios";
+import Axios from 'axios';
 import { MainContainer, Text } from './Main.styles';
-import { Form } from '../../components/Form'
+import { Form } from '../../components/Form';
 import { Input, CurrencyButton } from '../../components/Input';
 import { StyledDiv } from '../../components/Input/Input.styles';
-import { SelectComp } from '../../components/Select'
+import { SelectComp } from '../../components/Select';
 import { Textarea } from '../../components/Textarea';
 import { Button } from '../../components/Button';
 
@@ -13,79 +13,90 @@ export function Main() {
 	const [expense, setExpense] = useState('');
 	const [description, setDescription] = useState('');
 	const currencies = {
-			dollar: "USD",
-			peso: "COP",
-	}
-	const [activeCurrency, setActiveCurrency] = useState(currencies.dollar)
+		dollar: 'USD',
+		peso: 'COP',
+	};
+	const [activeCurrency, setActiveCurrency] = useState(currencies.dollar);
+	const [loading, setLoading] = useState(false);
 
 	function actualCurrency() {
-		if (activeCurrency === currencies.dollar) setActiveCurrency(currencies.peso) 
-		if (activeCurrency === currencies.peso) setActiveCurrency(currencies.dollar)
+		if (activeCurrency === currencies.dollar)
+			setActiveCurrency(currencies.peso);
+		if (activeCurrency === currencies.peso)
+			setActiveCurrency(currencies.dollar);
 	}
 
 	function clearInput() {
-		setCategory(null)
-		setExpense('')
-		setDescription('')
+		setCategory(null);
+		setExpense('');
+		setDescription('');
 	}
 
 	function checkForm() {
-		if ((category === null || category === "") && expense === "") alert("Please select category and input total expense")
-		else if (category === null || category === "") alert("Please select category")
-		else if (expense === "") alert("Please input total expense")
-		else postData()
+		if ((category === null || category === '') && expense === '')
+			alert('Please select category and input total expense');
+		else if (category === null || category === '')
+			alert('Please select category');
+		else if (expense === '') alert('Please input total expense');
+		else postData();
 	}
 
 	const postData = () => {
-		Axios.post("/api/submit", {
+		setLoading(true);
+		Axios.post('/api/submit', {
 			category: category.value,
-			expense: activeCurrency === currencies.dollar ? expense : ((expense/4800).toFixed(2)),
-			description: description
-		}).then(res => console.log('Posting data', res))
-			.catch(err => console.log(err))
-		
-	clearInput()
-	}
+			expense:
+				activeCurrency === currencies.dollar
+					? expense
+					: (expense / 4600).toFixed(2),
+			description: description,
+		})
+			.then((res) => {
+				console.log('Posting data', res);
+				clearInput();
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+				alert('error on submit, check console log');
+				setLoading(false);
+			});
+	};
 
 	return (
 		<MainContainer>
 			<Form
 				onSubmit={(e) => {
-					e.preventDefault()
+					e.preventDefault();
 				}}
 			>
+				<Text>Category</Text>
+				<SelectComp
+					defaultValue={category}
+					value={category}
+					onChange={(t) => {
+						setCategory(t);
+					}}
+				/>
 
-			<Text>Category</Text>
-			<SelectComp
-				defaultValue={category}
-				value={category}
-				onChange={(t) => {setCategory(t)}}					
-				/>
-				
-			<Text>Amount</Text>
-			<StyledDiv>
-				<Input
-					value={expense}
-					onChange={setExpense}
-				/>
-				<CurrencyButton
-					onClick={actualCurrency}	
+				<Text>Amount</Text>
+				<StyledDiv>
+					<Input value={expense} onChange={setExpense} />
+					<CurrencyButton onClick={actualCurrency}>
+						{activeCurrency}
+					</CurrencyButton>
+				</StyledDiv>
+
+				<Text>Description</Text>
+				<Textarea value={description} onChange={setDescription} />
+
+				<Button
+					onClick={() => {
+						checkForm();
+					}}
 				>
-				{activeCurrency}		
-				</CurrencyButton>				
-			</StyledDiv>
-				
-			<Text>Description</Text>
-			<Textarea
-				value={description}
-				onChange={setDescription}
-			/>
-				
-			<Button
-				onClick={() => {checkForm()}}
-			>
-			SUBMIT
-			</Button>
+					{loading ? 'Loading...' : 'SUBMIT'}
+				</Button>
 			</Form>
 		</MainContainer>
 	);
